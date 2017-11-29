d8c5fc5191b986fcf0aae836ac21be6b6febfd92
Hteew <viva.fidz@yandex.ru>
2017-11-29 08:22:40 +0300

    API_HOST вынесен в settings

--- WUI/mainapp/views.py
+++ WUI/mainapp/views.py
@@ -2,28 +2,30 @@ from django.shortcuts import render
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from requests.auth import HTTPBasicAuth
 import requests
-from .forms import ContactForm
+from .forms import ContactSupportForm
+from spp import settings
 
 
 def index(request):
     title = 'Главная'
-    sites = requests.get("http://shourick.pythonanywhere.com/sites").json()
-    persons = requests.get("http://shourick.pythonanywhere.com/persons").json()
+    sites = requests.get(settings.API_HOST + "sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
     return render(request, 'index.html', {'title': title, 'sites': sites, 'persons': persons})
 
 
 def keywords(request):
     title = 'Ключевые слова'
-    persons = requests.get("http://shourick.pythonanywhere.com/persons").json()
-    keywords = requests.get("http://shourick.pythonanywhere.com/keywords").json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    keywords = requests.get(settings.API_HOST + "keywords/", auth=HTTPBasicAuth('root', 'root_password')).json()
     return render(request, 'keywords.html', {'title': title, 'persons': persons, 'keywords': keywords})
 
 
 def support(request):
     title = 'Контакты'
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactSupportForm(request.POST)
         if form.is_valid():
             subject = form.cleaned_data['subject']
             sender = form.cleaned_data['sender']
@@ -39,21 +41,20 @@ def support(request):
                 return HttpResponse('Invalid header found')
             return render(request, 'email/thanks.html', {'title': 'Спасибо'})
     else:
-        form = ContactForm()
-    return render(request, 'email/support.html', {'title': title, 'form': form})
+        form = ContactSupportForm()
+        return render(request, 'email/support.html', {'title': title, 'form': form})
 
 
-@login_required
 def general(request):
     title = 'Общая статистика'
     if request.method == 'POST':
         person_id = request.POST.get('person_id')
-        url = "http://shourick.pythonanywhere.com/rank/" + str(person_id)
-        ranks = requests.get(url).json()
-        sites = requests.get("http://shourick.pythonanywhere.com/sites").json()
+        url = f"{settings.API_HOST}rank/{person_id}"
+        ranks = requests.get(url, auth=HTTPBasicAuth('root', 'root_password')).json()
+        sites = requests.get(settings.API_HOST + "sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'rank.html', {'title': title, 'ranks': ranks, 'sites': sites})
     else:
-        persons = requests.get("http://shourick.pythonanywhere.com/persons").json()
+        persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'general.html', {'title': title, 'persons': persons})
 
 
@@ -64,17 +65,15 @@ def rank(request):
 @login_required
 def daily(request):
     title = 'Ежедневная статистика'
-    persons = requests.get("http://shourick.pythonanywhere.com/persons").json()
-    sites = requests.get("http://shourick.pythonanywhere.com/sites").json()
+    persons = requests.get(settings.API_HOST + "persons/", auth=HTTPBasicAuth('root', 'root_password')).json()
+    sites = requests.get(settings.API_HOST +"sites/", auth=HTTPBasicAuth('root', 'root_password')).json()
     if request.method == 'POST':
         person_id = request.POST.get('person_id')
         site_id = request.POST.get('site_id')
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
-        url = "http://shourick.pythonanywhere.com/rank/" + str(person_id) + '/' + str(site_id) + '/' + \
-              str(start_date) + '&' + str(end_date)
-        rank = requests.get(url).json()
+        url = f"{settings.API_HOST}rank/{person_id}/{site_id}/{start_date}&{end_date}"
+        rank = requests.get(url, auth=HTTPBasicAuth('root', 'root_password')).json()
         return render(request, 'rank_daily.html', {'title': title, 'ranks': rank})
     else:
-        return render(request, 'daily.html', {'title': title, 'sites': sites, 'persons': persons})
-
+return render(request, 'daily.html', {'title': title, 'sites': sites, 'persons': persons})
